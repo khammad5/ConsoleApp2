025fa1505df24884152adbfad0b90ed7c10eef97
khammad5 <khammadakh@code.edu.az>
2023-10-14 09:08:56 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿#region task1
+//int start_num = 5;
+//int finish_num = 26;
+//int counter = 0;
+//for (int i = start_num; i < finish_num; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        counter++;
+//    }
+//}
+//Console.WriteLine(counter);
+
+#endregion
+
+#region task2
+//int num3 = 14;
+//if (num3 == 1)
+//{
+//    Console.WriteLine($"{num3} bu seda ve murekkebdir");
+//}
+//else if (num3 == 2 || num3 == 3)
+//{
+//    Console.WriteLine($"{num3} seda ededir");
+//}
+//else if (num3 % 2 == 0 || num3 % 3 == 0)
+//{
+//    Console.WriteLine($"{num3} murekkeb ededdir");
+//}
+//else
+//{
+//    Console.WriteLine($"{num3} sade ededdir");
+//}
+#endregion
+
+#region task3
+//int num5 = 32;
+//bool isPowerOf2 = true;
+
+//if (num5 <= 0)
+//{
+//    Console.WriteLine($"{num5} 2nin quvveti deyil");
+//}
+//else
+//{
+//    while (num5 > 1)
+//    {
+//        if (num5 % 2 != 0)
+//        {
+//            isPowerOf2 = false;
+//            break;
+//        }
+//        num5 /= 2;
+//    }
+
+//    if (isPowerOf2)
+//    {
+//        Console.WriteLine($"{num5} 2nin quvvetidir ");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"{num5} 2nin quvveti deyil");
+//    }
+
+//}
+#endregion
+
+#region task4
+//int num = 34546452;
+//int result = 0;
+//while (num > 0)
+//{
+//    result++;
+//    num /= 10;
+//}
+//Console.WriteLine(result);
+#endregion
+
+
